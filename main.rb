bea6a36086924de8a97312035dd92a45b9469c75
tonistark23 <toni.garcia@obf.ateneo.edu>
2014-05-11 21:09:37 +0800

    AM+DG
    Commit 1

---
+++ main.rb
@@ -0,0 +1,122 @@
+require 'sinatra'
+require './boot.rb'
+require './money_calculator.rb'
+
+# ROUTES FOR ADMIN SECTION
+get '/admin' do
+  @products = Item.all
+  erb :admin_index
+end
+
+get '/new_product' do
+  @product = Item.new
+  erb :product_form
+end
+
+post '/create_product' do
+if params[:name].empty? == true || params[:price].empty? == true || params[:quantity].empty? == true || params[:price].to_i < 0 || params[:quantity].to_i < 0 #assuming price and quantity can be 0 (free or not yet in stock)
+	erb :error
+else
+  Item.create!(
+    name: params[:name],
+    price: params[:price],
+    quantity: params[:quantity],
+    sold: 0
+  )
+  redirect to '/admin'
+  end
+end
+
+get '/edit_product/:id' do
+  begin #[1] RUBY EXCEPTIONS
+	  @product = Item.find(params[:id])
+	  erb :product_form
+  rescue
+	  erb :error
+  end
+end
+
+post '/update_product/:id' do
+  begin
+	  @product = Item.find(params[:id])
+  rescue
+	  erb :error
+  else
+	  if params[:name].empty? == true || params[:price].empty? == true || params[:quantity].empty? == true || params[:price].to_i < 0 || params[:quantity].to_i < 0 #assuming price and quantity can be 0 (free or not yet in stock)
+		erb :error
+      else
+		@product.update_attributes!(
+		name: params[:name],
+		price: params[:price],
+		quantity: params[:quantity],
+     	  )
+     	redirect to '/admin'
+     end
+  end
+end
+
+get '/delete_product/:id' do
+	begin
+	  @product = Item.find(params[:id])
+	  @product.destroy!
+	  redirect to '/admin'
+    rescue
+	  erb :error
+    end
+end
+# ROUTES FOR ADMIN SECTION
+
+#ROUTES FOR HOMEPAGE SECTION
+
+get '/' do
+	@products = Item.all
+	@random = @products.sample(10) #[2] Britt, J and Neurogami
+	erb :home
+end
+
+get '/products' do
+	@products = Item.all
+	erb :products
+end
+
+get '/about' do
+	erb :about
+end	
+
+get '/purchase_product/:id' do 
+	begin
+		@product = Item.find(params[:id]) #debug for amt > quantity. nothing negative
+		erb :purchase
+	rescue
+		erb :error
+	end
+end
+
+post '/purchase_product/:id' do 
+		if params[:id].empty? == true || params[:ones].empty? == true || params[:fives].empty? ==
+true || params[:tens].empty? == true || params[:twenties].empty? == true ||
+params[:fifties].empty? == true || params[:hundreds].empty? == true ||
+params[:five_hundreds].empty? == true || params[:thousands].empty? == true || params[:ones].to_i < 0 || params[:fives].to_i < 0 || params[:tens].to_i < 0 || params[:twenties].to_i < 0 ||
+params[:fifties].to_i < 0 || params[:hundreds].to_i < 0 || params[:five_hundreds].to_i < 0 ||
+params[:thousands].to_i < 0
+			erb :error
+		else
+			@product = Item.find(params[:id]) #debug for amt > quantity. nothing negative
+			@qSold = params[:amount].to_i
+			@calculator = MoneyCalculator.new(params[:ones], params[:fives], params[:tens], params[:twenties], params[:fifties], params[:hundreds], params[:five_hundreds], params[:thousands])
+			if @calculator.give_sum.to_i < (@qSold.to_i * @product.price.to_i)
+				erb :error
+			else
+				@product.update_attributes!( #compute for total price sold
+				name: @product.name,
+				price: @product.price,
+				quantity: @product.quantity - params[:amount].to_i,
+				sold: @product.sold.to_i + params[:amount].to_i,
+			  )	
+			  erb :tempo
+		end
+	end
+end
+	
+
+#ROUTES FOR HOMEPAGE SECTION
---
+++ money_calculator.rb
@@ -0,0 +1,105 @@
+class MoneyCalculator
+
+attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :five_hundreds, :thousands, :sum
+
+  def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
+    @ones = ones.to_i
+    @fives = fives.to_i
+    @tens = tens.to_i
+    @twenties = twenties.to_i
+    @fifties = fifties.to_i
+    @hundreds = hundreds.to_i
+    @five_hundreds = five_hundreds.to_i
+    @thousands = thousands.to_i
+    # each parameter represents the quantity per denomination of money
+    # these parameters can be assigned to instance variables and used for computation
+
+    # add a method called 'change' that takes in a parameter of how much an item costs
+    # and returns a hash of how much change is to be given
+    # the hash will use the denominations as keys and the amount per denomination as values
+    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
+  end
+  
+  def change(price)	  	
+  	@sum = @ones+(@fives*5)+(@tens*10)+(@twenties*20)+(@fifties*50)+(@hundreds*100)+(@five_hundreds*500)+(@thousands*1000)
+  	res = @sum.to_i - price.to_i
+  	@res = res
+  	@ones = 0
+  	@fives = 0
+    @tens = 0
+    @twenties = 0
+    @fifties = 0
+    @hundreds = 0
+    @five_hundreds = 0
+    @thousands = 0
+  	
+		while (res != 0)
+			if (res%5 == 0 and res%10 != 0 and res%20 != 0 and res%50 != 0 and res%100 !=0 and res%500 != 0 and res%1000 != 0)
+				@fives +=1
+				res = res- 5
+			elsif (res%10 == 0 and res%20 != 0 and res%50 != 0 and res%100 !=0 and res%500 != 0 and res%1000 != 0)
+				@tens+=1
+				res = res - 10	
+			elsif (res%20 == 0 and res%50 != 0 and res%100 !=0 and res%500 != 0 and res%1000 != 0)
+				@twenties+=1
+				res = res - 20
+			elsif (res%50 == 0 and res%100 !=0 and res%500 != 0 and res%1000 != 0)
+				@fifties+=1 
+				res = res -50
+			elsif (res%100 == 0 and res%500 != 0 and res%1000 != 0)
+				@hundreds+=1
+				res =res - 100
+			elsif (res%500 == 0 and res%1000 != 0)
+				@five_hundreds+=1
+				res = res - 500
+			elsif res%1000 == 0
+				@thousands +=1
+				res = res - 1000
+			else
+				@ones+=1
+				res-=1
+			end
+	  end
+  hsh = {
+  :ones =>@ones,
+  :fives => @fives,
+  :tens => @tens,
+  :twenties => @twenties,
+  :fifties => @fifties,
+  :hundreds => @hundreds,
+  :five_hundreds => @five_hundreds,
+  :thousands =>@thousands }
+  
+=begin
+hsh = {
+  :ones => 0
+  :fives => 0
+  :tens => 0
+  :twenties => 0
+  :fifties => 0
+  :hundreds => 0
+  :five_hundreds => 0
+  :thousands => 0 }
+=end
+
+	"Your change is:
+	  #{hsh[:ones]} one/s, 
+	  #{hsh[:fives]} five/s, 
+	  #{hsh[:tens]} ten/s, 
+	  #{hsh[:twenties]} twenty/ies, 
+	  #{hsh[:fifties]} fifty/ies, 
+	  #{hsh[:hundreds]} hundred/s, 
+	  #{hsh[:five_hundreds]} five hundred/s, and
+	  #{hsh[:thousands]} thousand/s. "	
+  
+  #hsh.each_pair { |key, value| puts "#{key}.to_s : #{value}"}
+  
+  end  
+  
+  def give_sum
+  	  	"#{@sum}"
+  end
+  
+
+  
+end   
